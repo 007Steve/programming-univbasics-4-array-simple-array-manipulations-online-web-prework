3d44593aa7865f5634b69324916c56aad6d97040
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2019-09-01 06:04:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -28,8 +28,27 @@ end
     array.shift(2)
 end
 
-def using_concat (my_favorite_things,more_favs)
-  
- all_my_favs=my_favorite_things.concat more_favs
+def using_concat(my_favorite_things, more_favs)
+  all_my_favs=my_favorite_things.concat more_favs
 end
+
+def using_insert(list_of_programming_languages, another_language)
+  new_array= list_of_programming_languages
+  new_array.insert(4,another_language)
+end
+
+def using_uniq(haircuts)
+  new_array=haircuts.uniq 
+end
+
+def using_flatten(instruments)
+  flat_array=instruments.flatten
+end  
+
+def using_delete(instructors, steven)
+  no_offense_steven=instructors.delete(steven)
+end
+
+def using_delete_at(array, integer)
+  array.delete_at(2)
 end
